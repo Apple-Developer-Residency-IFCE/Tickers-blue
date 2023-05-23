82b9c4650d6c3d392428dbad47f1dd05e48158d0
caio pessoa <caiopessoa1298@gmail.com>
2023-05-23 19:47:12 -0300

    project Tickers Create

---
+++ Tickers/Tickers/TickersApp.swift
@@ -0,0 +1,17 @@
+//
+//  TickersApp.swift
+//  Tickers
+//
+//  Created by userext on 23/05/23.
+//
+
+import SwiftUI
+
+@main
+struct TickersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
