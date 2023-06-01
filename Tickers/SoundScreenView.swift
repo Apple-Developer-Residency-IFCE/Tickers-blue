5e89397a2a3f0b37e51711f0817d4352e1525eaf
Julia Morales <juliamoraless1204@gmail.com>
2023-05-31 21:44:11 -0300

    Finalizado view da tela de sons

--- Tickers/SoundScreenView.swift
+++ Tickers/SoundScreenView.swift
@@ -10,30 +10,39 @@ import SwiftUI
 
 struct SoundScreenView: View {
     @State private var showingSounds = false
-    @State var ativado: String?
+    @State var selectedOption: String?
+    
     let options = ["Chuva", "Tempestade", "Água Corrente", "Lo-fi"]
-        
+    
     var body: some View {
         Button("Sons") {
             showingSounds.toggle()
         }
         .sheet(isPresented: $showingSounds) {
             VStack(alignment: .leading){
-                HStack{
+                HStack(spacing: 0){
                     Image("ButtonSons")
+                        .padding(.trailing, 12)
                     Text("Sons")
+                        .font(Font.tickerFont(font: .bold, size: .xxxl))
+                        .foregroundColor(.blue)
+                    Spacer()
+                    Image("ButtonExitPopups")
+                        .onTapGesture {
+                            showingSounds = false
+                        }
                 }
                 Divider()
                 ForEach(options, id: \.self) { option in
-                    RadioButtonView(text: option, isTapped: option == ativado)
+                    RadioButtonView(text: option, isTapped: option == selectedOption)
                         .onTapGesture {
-                            ativado = option
+                            selectedOption = option
                         }
                 }
             }
             .presentationDetents([.height(250)])
             .padding()
-            
+            .interactiveDismissDisabled()
         }
     }
 }
@@ -41,6 +50,6 @@ struct SoundScreenView: View {
 
 struct SoundScreenView_Previews: PreviewProvider {
     static var previews: some View {
-        SoundScreenView(ativado: "sss")
+        SoundScreenView(selectedOption: "sss")
     }
 }
