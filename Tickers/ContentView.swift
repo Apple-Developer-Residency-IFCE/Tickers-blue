9521b3b9f0b685c7df894f2e18bc830531ee5dc7
Caio Pessoa <49486881+caio11pessoa@users.noreply.github.com>
2023-06-02 19:03:22 -0300

    mergin

--- Tickers/ContentView.swift
+++ Tickers/ContentView.swift
@@ -8,16 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                        Image(systemName: "globe")
+                            .imageScale(.large)
+                            .foregroundColor(.accentColor)
+                        Text("Hello, world!")
+                    }
+                    .padding()
         }
-        .padding()
     }
-}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
--- Tickers/Tickers/TickersApp.swift
+++ Tickers/Tickers/TickersApp.swift
@@ -1,17 +1,11 @@
-//
-//  TickersApp.swift
-//  Tickers
-//
-//  Created by userext on 23/05/23.
-//
 
 import SwiftUI
 
 @main
 struct TickersApp: App {
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
         }
     }
 }
