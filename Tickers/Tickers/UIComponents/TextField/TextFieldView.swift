ba9b792fe600241b082cbe9989ee1ae61b9ca186
Vanessa Alencar <vanessa2geo@gmail.com>
2023-06-13 20:38:03 -0300

    alteracoes binding

--- Tickers/Tickers/UIComponents/TextField/TextFieldView.swift
+++ Tickers/Tickers/UIComponents/TextField/TextFieldView.swift
@@ -12,32 +12,45 @@ let customFont = Font.tickerFont(font: .bold, size: .extraLarge)
 let customComent =  Font.tickerFont(font: .regular, size: .small)
 
 struct TextFieldView: View {
-    @State private var nome: String = ""
+    let title: String
+    let subtitle: String
+    @Binding var nome: String
+    
     var body: some View {
         VStack(alignment: .leading){
-            Text("Seu nome")
+            Text(title)
                 .font(customFont)
                 .foregroundColor(.blue)
             
-                Text("Define como os tickers chamarão você")
-                    .font(customComent)
-                    .padding(.bottom)
-                
-                    TextField("Escreva aqui seu nome", text: $nome)
+            Text(subtitle)
+                .font(customComent)
+                .padding(.bottom)
+            
+            TextField("Escreva aqui seu nome", text: $nome)
                 .padding(.leading, 10)
                 .overlay {
                     RoundedRectangle(cornerRadius: 20)
                         .stroke(Color.gray, lineWidth: 0.5)
                         .frame(height: 50)
                 }
-                
             
         }.padding(10)
     }
 }
-    struct TextFieldView_Previews: PreviewProvider {
-        static var previews: some View {
-            TextFieldView()
-        }
+
+fileprivate struct TextFieldToPreview: View {
+    @State var text: String = ""
+    
+    var body: some View {
+        TextFieldView(title: "Seu nome",
+                      subtitle: "Define como os tickers chamarão você",
+                      nome: $text)
     }
+}
+
+struct TextFieldView_Previews: PreviewProvider {
+    static var previews: some View {
+        TextFieldToPreview()
+    }
+}
 
