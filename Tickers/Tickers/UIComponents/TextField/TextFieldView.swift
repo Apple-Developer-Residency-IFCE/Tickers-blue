6c28ae7f39bda95c80eae925104c6814e1704d04
Vanessa Alencar <vanessa2geo@gmail.com>
2023-06-12 21:32:16 -0300

    criacao text field

--- Tickers/Tickers/UIComponents/TextField/TextFieldView.swift
+++ Tickers/Tickers/UIComponents/TextField/TextFieldView.swift
@@ -7,14 +7,37 @@
 
 import SwiftUI
 
-struct TextFieldView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
 
-struct TextFieldView_Previews: PreviewProvider {
-    static var previews: some View {
-        TextFieldView()
+let customFont = Font.tickerFont(font: .bold, size: .extraLarge)
+let customComent =  Font.tickerFont(font: .regular, size: .small)
+
+struct TextFieldView: View {
+    @State private var nome: String = ""
+    var body: some View {
+        VStack(alignment: .leading){
+            Text("Seu nome")
+                .font(customFont)
+                .foregroundColor(.blue)
+            
+                Text("Define como os tickers chamarão você")
+                    .font(customComent)
+                    .padding(.bottom)
+                
+                    TextField("Escreva aqui seu nome", text: $nome)
+                .padding(.leading, 10)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.gray, lineWidth: 0.5)
+                        .frame(height: 50)
+                }
+                
+            
+        }.padding(10)
     }
 }
+    struct TextFieldView_Previews: PreviewProvider {
+        static var previews: some View {
+            TextFieldView()
+        }
+    }
+
