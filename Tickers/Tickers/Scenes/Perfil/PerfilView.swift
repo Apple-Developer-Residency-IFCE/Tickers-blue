47bbfb4d8460cf55afc735e44acde457cb18fda6
caio pessoa <caiopessoa1298@gmail.com>
2023-06-16 21:16:53 -0300

    ajustando navigation perfil

--- Tickers/Tickers/Scenes/Perfil/PerfilView.swift
+++ Tickers/Tickers/Scenes/Perfil/PerfilView.swift
@@ -11,7 +11,7 @@ struct PerfilView: View {
     var body: some View {
         NavigationView{
             Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-                .navigationTitle("Caio")
+                .navigationTitle("Perfil")
         }
         
     }
